cf3fb52c8002be3a32bea5f736bef78289fea23a
keithpohl82 <keith.pohl82@gmail.com>
2024-08-23 13:07:00 -0500

    Exercise 3

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Game Development</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>There are many game engines people can use to develop games</li>
+            <li>The most popular are Unreal Engine and Unity</li>
+            <li>Game development can be on many different platforms or many at once</li>
          </ol>
       </div>
    );
